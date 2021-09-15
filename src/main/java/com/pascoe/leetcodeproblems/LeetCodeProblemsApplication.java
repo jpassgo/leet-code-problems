f3a52f57df6c4b48de5c38c2eca19f59ce9f2e5d
jpassgo <jeffpascoe9@gmail.com>
2021-09-14 20:40:43 -0500

    Spring app commit.

--- src/main/java/com/pascoe/leetcodeproblems/LeetCodeProblemsApplication.java
+++ src/main/java/com/pascoe/leetcodeproblems/LeetCodeProblemsApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LeetCodeProblemsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LeetCodeProblemsApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(LeetCodeProblemsApplication.class, args);
+  }
 }
