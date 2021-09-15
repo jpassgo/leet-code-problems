2bcc3a9f0027889ddec29ae42c584bb4b5521a1a
jpassgo <jeffpascoe9@gmail.com>
2021-09-14 20:41:30 -0500

    A leet code problem.

---
+++ src/main/java/com/pascoe/leetcodeproblems/solutions/design/shuffle_an_array/ShuffleAnArray.java
@@ -0,0 +1,26 @@
+package com.pascoe.leetcodeproblems.solutions.design.shuffle_an_array;
+
+public class ShuffleAnArray {
+
+  /**
+   * https://leetcode.com/explore/featured/card/top-interview-questions-easy/98/design/670/
+   *
+   * <p>Given an integer array nums, design an algorithm to randomly shuffle the array. All
+   * permutations of the array should be equally likely as a result of the shuffling.
+   *
+   * <p>Implement the Solution class:
+   *
+   * <p>Solution(int[] nums) Initializes the object with the integer array nums. int[] reset()
+   * Resets the array to its original configuration and returns it. int[] shuffle() Returns a random
+   * shuffling of the array.
+   */
+
+  public static void main(String[] args) {
+    var solution = new Solution(new int[]{1, 2, 3, 4, 5});
+
+    int[] shuffle = solution.shuffle();
+    for (int i = 0; i < shuffle.length - 1; i++) {
+      System.out.print(shuffle[i] + " ");
+    }
+  }
+}
