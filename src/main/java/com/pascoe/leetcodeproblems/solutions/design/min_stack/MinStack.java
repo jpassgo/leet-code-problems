950ed33e985f5b6241c967f6becb6ad656982267
jpassgo <jeffpascoe9@gmail.com>
2021-09-18 13:26:10 -0500

    Properly solving the leet code problem. The min heap internal implementation was overkill.

--- src/main/java/com/pascoe/leetcodeproblems/solutions/design/min_stack/MinStack.java
+++ src/main/java/com/pascoe/leetcodeproblems/solutions/design/min_stack/MinStack.java
@@ -1,42 +1,45 @@
 package com.pascoe.leetcodeproblems.solutions.design.min_stack;
 
+import lombok.AllArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
 public class MinStack<T extends Comparable> {
 
   /**
    * Internally implement the stack as a min heap. For each node associate a value and a key that
    * represents the node's place within the virtual stack.
    */
-  private T[] heap;
+  private List<T> stack;
+  private T currentMinElement;
 
-  class Node<T extends Comparable> {
-    private T val;
-    private int position;
-
-    public Node(T val, int position) {
-      this.val = val;
-      this.position = position;
-    }
-
-    public T getVal() {
-      return val;
-    }
-
-    public void setVal(T val) {
-      this.val = val;
-    }
-
-    public int getPosition() {
-      return position;
-    }
-
-    public void setPosition(int position) {
-      this.position = position;
-    }
+  public MinStack() {
+    this.stack = new ArrayList();
   }
 
   public void push(T value) {
-    for (int i = 0; i < heap.length - 1; i++) {
-      if (value.compareTo(heap[i]) < 0) {}
+    if(currentMinElement == null) {
+      currentMinElement = value;
+    } else if(value.compareTo(currentMinElement) < 0) {
+      currentMinElement = value;
     }
+    stack.add(value);
   }
+
+  public T pop() {
+    T t = stack.get(stack.size() - 1);
+    stack.remove(t);
+    return t;
+  }
+
+  public T top() {
+    return stack.get(stack.size() - 1);
+  }
+
+  public T getMin() {
+    return currentMinElement;
+  }
+
 }
