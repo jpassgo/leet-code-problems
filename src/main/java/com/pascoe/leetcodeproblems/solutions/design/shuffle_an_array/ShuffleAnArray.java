a0e157a729821eb90ad483e85aa15d4fa32cf5e3
jpassgo <jeffpascoe9@gmail.com>
2021-09-20 20:34:58 -0500

    Spotless

--- src/main/java/com/pascoe/leetcodeproblems/solutions/design/shuffle_an_array/ShuffleAnArray.java
+++ src/main/java/com/pascoe/leetcodeproblems/solutions/design/shuffle_an_array/ShuffleAnArray.java
@@ -14,7 +14,6 @@ public class ShuffleAnArray {
    * Resets the array to its original configuration and returns it. int[] shuffle() Returns a random
    * shuffling of the array.
    */
-
   public static void main(String[] args) {
     int[] ints = {1, 2, 3, 4, 5};
     var solution = new Solution(ints);
--- src/main/java/com/pascoe/leetcodeproblems/solutions/design/shuffle_an_array/Solution.java
+++ src/main/java/com/pascoe/leetcodeproblems/solutions/design/shuffle_an_array/Solution.java
@@ -2,42 +2,42 @@ package com.pascoe.leetcodeproblems.solutions.design.shuffle_an_array;
 
 public class Solution {
 
-    private int[] nums;
+  private int[] nums;
 
-    public Solution(int[] nums) {
-        this.nums = nums;
+  public Solution(int[] nums) {
+    this.nums = nums;
+  }
+
+  int[] reset() {
+    return new int[10];
+  }
+
+  int[] shuffle() {
+    int numsLength = nums.length;
+    int[] tempArray = new int[numsLength];
+    for (int i = 0; i < numsLength; i++) {
+      int index = randomIntInRange(i, numsLength);
+      if (tempArray[index] == 0) {
+        tempArray[index] = nums[i];
+      } else {
+        tempArray[nextEmptySlot(tempArray, 0)] = nums[i];
+      }
     }
 
-    int[] reset() {
-        return new int[10];
+    this.nums = tempArray;
+
+    return nums;
+  }
+
+  int nextEmptySlot(int[] array, int index) {
+    if (array[index] == 0) {
+      return index;
     }
+    return nextEmptySlot(array, ++index);
+  }
 
-    int[] shuffle() {
-        int numsLength = nums.length;
-        int[] tempArray = new int[numsLength];
-        for (int i = 0; i < numsLength; i++) {
-            int index = randomIntInRange(i, numsLength);
-            if(tempArray[index] == 0) {
-                tempArray[index] = nums[i];
-            } else {
-                tempArray[nextEmptySlot(tempArray, 0)] = nums[i];
-            }
-        }
-
-        this.nums = tempArray;
-
-        return nums;
-    }
-
-    int nextEmptySlot(int[] array, int index) {
-        if(array[index] == 0) {
-            return index;
-        }
-        return nextEmptySlot(array, ++index);
-    }
-
-    // min and max are exclusive bounds
-    private int randomIntInRange(int min, int max) {
-        return (int) ((Math.random() * (max - min)) + min);
-    }
- }
+  // min and max are exclusive bounds
+  private int randomIntInRange(int min, int max) {
+    return (int) ((Math.random() * (max - min)) + min);
+  }
+}
