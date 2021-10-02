b499e50a4c42f15f354358eedb2cc6c8250ec5d5
jpassgo <jeffpascoe9@gmail.com>
2021-10-01 23:51:46 -0500

    Starting leetcode problem.

---
+++ src/main/java/com/pascoe/leetcodeproblems/solutions/dynamic_programming/climbing_stairs/ClimbingStairs.java
@@ -0,0 +1,23 @@
+package com.pascoe.leetcodeproblems.solutions.dynamic_programming.climbing_stairs;
+
+public class ClimbingStairs {
+
+    /**
+     *
+     * https://leetcode.com/explore/featured/card/top-interview-questions-easy/97/dynamic-programming/569/
+     *
+     * You are climbing a staircase. It takes n steps to reach the top.
+     *
+     * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+     *
+     * @param args
+     */
+
+  public static void main(String[] args) {
+
+  }
+
+  public int waysToClimbNStairs(int numberOfStairs) {
+
+  }
+}
