74f7f8c1aa30ca75386cd635852a13323da639b3
jpassgo <jeffpascoe9@gmail.com>
2021-10-02 21:52:36 -0500

    Failed implementation. Work in progress.

--- src/main/java/com/pascoe/leetcodeproblems/solutions/dynamic_programming/climbing_stairs/ClimbingStairs.java
+++ src/main/java/com/pascoe/leetcodeproblems/solutions/dynamic_programming/climbing_stairs/ClimbingStairs.java
@@ -1,5 +1,8 @@
 package com.pascoe.leetcodeproblems.solutions.dynamic_programming.climbing_stairs;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
 public class ClimbingStairs {
 
     /**
@@ -14,10 +17,35 @@ public class ClimbingStairs {
      */
 
   public static void main(String[] args) {
-
+      System.out.println(waysToClimbNStairs(1));
+      System.out.println(waysToClimbNStairs(2));
+      System.out.println(waysToClimbNStairs(3));
+      System.out.println(waysToClimbNStairs(4));
   }
 
-  public int waysToClimbNStairs(int numberOfStairs) {
-
+  public static int waysToClimbNStairs(int numberOfStairs) {
+      return waysToClimbNStairs(numberOfStairs, new Integer[]{});
   }
+
+  private static Integer waysToClimbNStairs( int index, Integer[] memo) {
+      if(index == 1 || index == 2 || index == 3) {
+          return index;
+      }
+
+      if(isValueMemoized(memo, index)) {
+          return memo[index];
+      } else {
+          memo[index] = memo[index - 1] + memo[index - 2];
+      }
+      return memo[index];
+  }
+
+    private static boolean isValueMemoized(Integer[] memo, Integer index) {
+        try {
+            if(memo[index] != null) return true;
+        } catch(Exception e) {
+            return false;
+        }
+        return false;
+    }
 }
