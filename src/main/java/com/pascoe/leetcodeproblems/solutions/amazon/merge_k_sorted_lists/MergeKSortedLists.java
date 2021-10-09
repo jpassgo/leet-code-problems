df4de10becb7da72b4db8d774e85f73893904a7c
jpassgo <jeffpascoe9@gmail.com>
2021-10-09 14:50:51 -0500

    spotless

--- src/main/java/com/pascoe/leetcodeproblems/solutions/amazon/merge_k_sorted_lists/MergeKSortedLists.java
+++ src/main/java/com/pascoe/leetcodeproblems/solutions/amazon/merge_k_sorted_lists/MergeKSortedLists.java
@@ -2,20 +2,19 @@ package com.pascoe.leetcodeproblems.solutions.amazon.merge_k_sorted_lists;
 
 public class MergeKSortedLists {
 
-    /**
-     * https://leetcode.com/problems/merge-k-sorted-lists/
-     *
-     * 23. Merge k Sorted Lists
-     *
-     * You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
-     *
-     * Merge all the linked-lists into one sorted linked-list and return it.
-     *
-     *
-     * @param args
-     */
-
-    public static void main(String[] args) {
-        Integer[][] lists = new Integer[][]{{1,4,5},{1,3,4},{2,6}};
-    }
+  /**
+   * https://leetcode.com/problems/merge-k-sorted-lists/
+   *
+   * <p>23. Merge k Sorted Lists
+   *
+   * <p>You are given an array of k linked-lists lists, each linked-list is sorted in ascending
+   * order.
+   *
+   * <p>Merge all the linked-lists into one sorted linked-list and return it.
+   *
+   * @param args
+   */
+  public static void main(String[] args) {
+    Integer[][] lists = new Integer[][] {{1, 4, 5}, {1, 3, 4}, {2, 6}};
+  }
 }
